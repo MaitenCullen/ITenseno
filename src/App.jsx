c39f5fb595c4cd66f101aa00295f7961804a7289
Maiten Cullen <maitencullen@gmail.com>
2022-11-17 17:30:36 -0300

    cambia navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import DrawerAppBar from './components/NavBar';
 import { Footer } from './components/Footer';
 import { Home } from './screens/Home';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div className="App">
-      <DrawerAppBar/>
+      <NavBar/>
       <Home/>
       <Footer/>
     </div>
