400dabf60a2f5efe1a7fb03a686fce35b226a871
oswaldonaupa <oswaldo.naupa@tecsup.edu.pe>
2022-11-17 19:23:35 -0500

    feat: resolve conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { NavBar } from './components/NavBar';
 import Footer from './components/Footer';
-
-import { Footer } from './components/Footer';
 import { Home } from './screens/Home';
 import NavBar from './components/NavBar';
 
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,27 +1,21 @@
 import React from 'react'
-import { CartAbout } from '../components/CartAbout'
-import { CartButtons } from '../components/CartButtons'
-import { CartData } from '../components/CartData'
 import { CartHome } from '../components/CartHome'
 import { CartImages } from '../components/CartImages'
 import { SectionFind } from '../components/home/SectionFind'
 import { SectionTechnologies } from '../components/home/SectionTechnologies'
+import AboutUs from '../components/AboutUs'
 
 export const Home = () => {
   return (
     <>
       <CartHome/>
+      <SectionTechnologies/>
+      <SectionFind/>
 
-    <CartButtons/>
+      <CartImages/>
 
-    <CartData/>
-
-    <CartImages/>
-
-    <CartAbout/>
-
-    <SectionTechnologies/>
-    <SectionFind/>
+      <AboutUs/>
+  
     </>
 
   )
