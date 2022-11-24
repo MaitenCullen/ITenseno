25b875088115b4ac6c55a8fa343545c1961fe841
Pedro Fernandez <pefernandez1408@gmail.com>
2022-11-23 20:59:21 -0400

    pull de la rama main

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
-<<<<<<< HEAD
-import  AboutUs  from '../components/AboutUs'
-import { CartButtons } from '../components'
-import { CartData } from '../components'
-import { CartHome } from '../components'
-import { CartImages } from '../components'
-import { SectionFind } from '../components/home'
-import { SectionTechnologies } from '../components/home'
-=======
+
 import { CartHome } from '../components/CartHome'
 import { CartImages } from '../components/CartImages'
 import { SectionFind } from '../components/home/SectionFind'
 import { SectionTechnologies } from '../components/home/SectionTechnologies'
 import AboutUs from '../components/AboutUs'
->>>>>>> 0e89b916cb319a2f5f2b1bbe7353ce86159e669b
 
 export const Home = () => {
   return (
@@ -24,18 +15,8 @@ export const Home = () => {
 
       <CartImages/>
 
-<<<<<<< HEAD
-    <CartData/>
 
-    <CartImages/>
-
-    <AboutUs/>
-
-    <SectionTechnologies/>
-    <SectionFind/>
-=======
       <AboutUs/>
->>>>>>> 0e89b916cb319a2f5f2b1bbe7353ce86159e669b
     </>
 
   )
