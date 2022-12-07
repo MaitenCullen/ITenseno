0dbbc32800046a299729f9e08d88a8b5fe1aad35
oswaldonaupa <oswaldo.naupa@tecsup.edu.pe>
2022-12-06 21:36:10 -0500

    feat: profile public

--- src/screens/ProfilePublic.jsx
+++ src/screens/ProfilePublic.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { CardClass } from '../components/CardClass'
 import { CardTechnologie } from '../components/CardTechnologie'
 import { useParams } from "react-router-dom";
 
 import star from "../assets/star.png";
+import starColor from "../assets/star_color.png";
+import { getProfile } from '../utilities/servicies';
 
 const ProfilePublic = () => {
 
     const { id } = useParams()
 
     console.log(id);
+
+    const [user, setUser] = useState(null)
+    
+    useEffect (() => {
+        getProfile(id)
+        .then((resp) => {
+            console.log(resp, "soy la tecnologia")
+            setUser(resp.user);
+         })
+    },[]);
 
 
   return (
@@ -18,33 +30,33 @@ const ProfilePublic = () => {
             <button>CONTACTAR</button>
         </div>
         <div className="profile-public__body">
-            <div className="profile-public__body__profile">
+            {user && <div className="profile-public__body__profile">
                 <div className="profile-public__body__profile__data">
                     <div className="profile-public__body__profile__data__left">
                         <img className='profile-image' src='https://learnenglish.britishcouncil.org/sites/podcasts/files/2021-10/RS6715_492969113-hig.jpg'/>
                         <div className="profile-public__body__profile__data__star">
-                            <img src={star} alt="star" />
-                            <img src={star} alt="star" />
-                            <img src={star} alt="star" />
-                            <img src={star} alt="star" />
-                            <img src={star} alt="star" />
+                            <img src={user.points.Averange < 1 ? star : starColor} alt="star" />
+                            <img src={user.points.Averange < 2 ? star : starColor} alt="star" />
+                            <img src={user.points.Averange < 3 ? star : starColor} alt="star" />
+                            <img src={user.points.Averange < 4 ? star : starColor} alt="star" />
+                            <img src={user.points.Averange < 5 ? star : starColor} alt="star" />
                         </div>
                     </div>
                     <div className="profile-public__body__profile__data__right">
-                        <h3>Sofía Muro</h3>
-                        <h3>Front-End Developer</h3>
+                        <h3>{user.firstName} {user.lastName}</h3>
+                        <h3>{user.tech}</h3>
                         <h5>Principales tecnologías</h5>
                         <div className="profile-public__body__profile__data__technologie">
-                            <CardTechnologie name={'Tecnoogia 1'} level={'Avanzado'}/>
-                            <CardTechnologie name={'Tecnoogia 1'} level={'Avanzado'}/>
-                            <CardTechnologie name={'Tecnoogia 1'} level={'Avanzado'}/>
+                            <CardTechnologie name={user.tech} level={'Avanzado'}/>
+                            {/*<CardTechnologie name={user.tech} level={'Avanzado'}/>
+                            <CardTechnologie name={user.tech} level={'Avanzado'}/>*/}
                         </div>
                     </div>
                    
                 </div>
                 <h5>Acerca de mí</h5>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ex quas, error delectus aliquam veniam non facere voluptate quis numquam. Doloremque non facere ex tenetur autem quidem, voluptatem accusantium nam!.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam ex quas, error delectus aliquam veniam non facere voluptate quis numquam. Doloremque non facere ex tenetur autem quidem, voluptatem accusantium nam!</p>
-            </div>
+                <p>{user.aboutMe}</p>
+            </div>}
             <div className="profile-public__body__classes">
                 <h3>Próximas clases</h3>
                 <CardClass name={'Clase A'} level={'NIVEL AVANZADO'} description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
